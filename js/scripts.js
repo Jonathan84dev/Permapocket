9e84cc23e3f870591637b144b7695dbde5164d76
Pénélope PRINGALLE <penelopepringalle@gmail.com>
2020-07-20 19:55:48 +0200

    improve dragula- first attempt

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@
 // 3.Possibilité de retour au formulaire
 // 4. validation du template et passage au drag and drop
 
+// diverses constantes
 const formulaire = document.getElementById('formulaire');
 const newGarden = document.getElementById('creationTable');
 const largeur = document.getElementById('largeur');
@@ -12,9 +13,13 @@ const generation = document.getElementById('generation');
 const returnCreation = document.getElementById('retourCreation');
 const validationGrille = document.getElementById('validationGrille');
 
+//création des tableaux de fruits et légumes
+
 const fruits = ['fraise', 'groseille', 'framboise', 'cassis', 'melon', 'pastèque'];
 const legumes = ['aubergine', 'courgette', 'carotte', 'tomate', 'pommes-de-terre', 'ail', 'oignon', 'echalotte'];
 const herbes = ['thym', 'basilic', 'ciboulette', 'romarin', 'persil', 'estragon', 'cerfeuil', 'melisse', 'menthe', 'sauge'];
+
+//Récupération des données du formulaire: au click, génération de la grille et apparition bouton de validation de la grille + disparition du formulaire et des boutons  
 
 generation.addEventListener('click', () => {
   event.preventDefault();
@@ -39,36 +44,38 @@ generation.addEventListener('click', () => {
 });
 
 validationGrille.addEventListener('click', () => {
-  document.getElementById('plantes').classList.add('d-md-block');
-  
 
-jQuery(document).ready(($) => {
+  //Au clic de validation de grille: apparition de la div contenant les imagges  
+  document.getElementById('plantes').classList.add('d-block');
+
+  //+chargement d'isotope
   const $grid = jQuery('.grid');
 
   $grid.isotope({
-    
-      itemSelector: '.grid-item',
-      layoutMode: 'masonry',
-      getSortData: {
-          name: '.name',
-          category: '[data-category]'
-      }
-  });
-});
 
+    itemSelector: '.grid-item',
+    layoutMode: 'masonry',
+    getSortData: {
+      name: '.name',
+      category: '[data-category]'
+    }
+  });
+
+  //disparition des boutons la page précédente
   returnCreation.classList.add('d-none');
   validationGrille.classList.add('d-none');
+
+  //apparition des boutons d'impression et de sauvegarde 
   document.getElementById('print').classList.add('d-block');
   document.getElementById('pdfSave').classList.add('d-block');
-  document.getElementsByClassName('explicationVersionDesktop')[0].classList.add('d-block');
-  if (document.getElementById('exampleModal')) {
-    document.getElementById('exampleModal').on('show.bs.modal', event => {
-      var button = $(event.relatedTarget);
-      var modal = $(this);
-    });
-  }
 
-document.getElementById('modelId').addEventListener('click', () => {
+  //apparition de la modale d'explication
+  document.getElementsByClassName('explicationVersionDesktop')[0].classList.add('d-block');
+
+
+
+  //au clic sur les boutons de la modale ou en-dehors, disparition de la modale
+  document.getElementById('modelId').addEventListener('click', () => {
     document.getElementById('modelId').classList.remove('d-block');
   });
 
@@ -76,38 +83,89 @@ document.getElementById('modelId').addEventListener('click', () => {
 
   //Dragula
 
-  var drake = dragula({copy:true});
-  const td = document.getElementsByClassName('td');
+  // var drake = dragula({ copy: true });
+  // const td = document.getElementsByClassName('td');
+
+  // const tableauTd = [];
+  // for (let i in td) {
+  //   drake.containers.push(td[i]);
+  //   tableauTd.push(td[i]);
+  // };
+
+  // const figures = document.getElementsByTagName('figure');
+  // console.log(figures);
+  // const tabTwo = [];
+  // for (let j in figures) {
+  //   drake.containers.push(figures[j]);
+  //   // tableauTd.push(figures[j]);
+  //   tabTwo.push(figures[j]);
+  // };
+
+
+  // console.log(tableauTd);
+  /*Pour drag and drop + copy*/
+  // var drake = dragula([tableauTd, tabTwo], { copy: true });
+
+  /*Pour effacer d'un conteneur*/
+  // var effacerImage = dragula(tableauTd, { removeOnSpill: true });
+
+//   drake.on('drag', function () {
+//     console.log('test drake');
+//   })
+//   effacerImage.on('drag', function (el, target, source, sibling) {
+//     console.log('test effacer');
+//   });
+
+
   
+// });
+
+ //Dragula
+
+  //initialise drake
+  var drake = dragula({ copy: true });
+  //get the td's and figures element
+  const td = document.getElementsByClassName('td');
+  const figures = document.getElementsByTagName('figure');
+
+  //push td's and figure's elements into drake's container
   const tableauTd = [];
   for (let i in td) {
     drake.containers.push(td[i]);
     tableauTd.push(td[i]);
-};
+  };
 
-const figures = document.getElementsByTagName('figure');
-console.log(figures);
-const tabTwo = [];
   for (let j in figures) {
     drake.containers.push(figures[j]);
-    // tableauTd.push(figures[j]);
-    tabTwo.push(figures[j]);
-};
- 
+  };
 
-console.log(tableauTd);
-/*Pour drag and drop + copy*/
-var drake = dragula([tableauTd, tabTwo], { copy: true });
+  drake.containers.push(tableauTd);
+  const effacer = dragula(tableauTd, {removeOnSpill : true});
 
-/*Pour effacer d'un conteneur*/
-var effacerImage = dragula(tableauTd, { removeOnSpill: true});
+  //console.log(drake.containers);
 
-drake.on('drag', function () {
-  console.log('test drake');
-})
-effacerImage.on('drag', function (el, target, source, sibling) {
-  console.log('test effacer');
-});
+  //on drop event, if target container is already full, old image is removed
+  drake.on('drop', function (el, target, source, sibling) {
+    console.log('tada');
+    console.log(el);
+    console.log(target);
+    console.log(source);
+    console.log(sibling);
+    console.log(source.tagName);
+    console.log(target.children.length);
+    console.log(target.children);
 
+    for (let i of target.children) {
+
+      if (!i.classList.contains('gu-transit')) {
+         console.log(i);
+        i.remove();
+       
+      } 
+      if((source.tagName==='DIV' || source.tagName==='FIGURE') && (target.tagName==='FIGURE' ||target.tagName==='FIGCAPTION')){
+          i.cancel();
+        }
+    } 
+  });
 });
 
